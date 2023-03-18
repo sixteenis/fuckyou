c8b1b7b48ad0790be847d9f397188e9f01bb59d5
sixteenis <psm0103907@naver.com>
2023-03-19 00:35:43 +0900

    Initial Commit

---
+++ fuckyou/fuckyouApp.swift
@@ -0,0 +1,17 @@
+//
+//  fuckyouApp.swift
+//  fuckyou
+//
+//  Created by 박성민 on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct fuckyouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
